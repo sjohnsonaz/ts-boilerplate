97ef45c35386244544c5a480ec7ac197ce86140b
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-06-06 12:40:32 -0700

    Updated dependencies.

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -8,3 +8,18 @@ describe('Application.run', () => {
         expect(application.run()).to.equal('started');
     });
 });
+
+describe('Dom', () => {
+    it('should have window', () => {
+        expect(window).to.not.be.undefined;
+    });
+
+    it('should have document', () => {
+        expect(document).to.not.be.undefined;
+    });
+
+    it('should have document.createElement', () => {
+        var element = document.createElement('div');
+        expect(element.tagName.toLowerCase()).to.equal('div');
+    });
+});
